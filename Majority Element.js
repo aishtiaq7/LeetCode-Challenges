7db737329a12c8280bc274f7c080dbc54cc78510
aishtiaq <awshaf_ishtiaque@sfu.ca>
2023-12-12 02:21:45 -0800

    update

--- Majority Element.js
+++ Majority Element.js
@@ -5,6 +5,7 @@
 var majorityElement = function (nums) {
   console.log(nums);
   let ht = {};
+  // make the frequncy table for nums in ht
   for (const char in nums) {
     if (ht[nums[char]] === undefined) {
       ht[nums[char]] = 1;
@@ -14,20 +15,21 @@ var majorityElement = function (nums) {
   }
   console.log("ht:", ht);
 
+  // find the maxOccurance and for which element(majorityElement)
   let maxOccurance = -Infinity;
   let majorityElement = -Infinity;
   for (const [key, value] of Object.entries(ht)) {
     if (value > maxOccurance) {
       maxOccurance = value;
       majorityElement = key;
-      console.log('key:',key)
+      console.log("key:", key);
     }
   }
-  console.log('maxCount: ', maxOccurance);
-  console.log('majorityElement: ', majorityElement);
-  return majorityElement
+  console.log("maxCount: ", maxOccurance);
+  console.log("majorityElement: ", majorityElement);
+  return majorityElement;
 };
 
 console.clear();
-const nums = [1, 2, 1,3,1,4,5]
+const nums = [1, 2, 1, 3, 1, 4, 5];
 majorityElement(nums);
